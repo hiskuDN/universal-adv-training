c76bb1547b68892ecd1a028f85d40375a390ab6f
Hisku <hiskias.melke1@gmail.com>
2024-11-20 14:18:27 +0900

    refactor: remove unused import of input_data from train_zico.py

--- train_zico.py
+++ train_zico.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import os
-from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 tf.disable_v2_behavior()
 import numpy as np
