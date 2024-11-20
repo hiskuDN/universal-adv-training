194c00024f9728d1f740aa3e751d5ba721f52ddc
Hisku <hiskias.melke1@gmail.com>
2024-11-20 14:19:38 +0900

    refactor: update TensorFlow import to use compatibility mode

--- train_zico.py
+++ train_zico.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import os
-import tensorflow as tf
-tf.disable_v2_behavior()
+import tensorflow.compat.v1 as tf
+tf.compat.v1.disable_v2_behavior()
 import numpy as np
 
 class ZicoMNIST(object):
